e0cc65ef3c6b611caf6b051716502ea74edae89e
Adarshmcherupuzha <adarshmpadichal@gmail.com>
2024-02-08 15:51:56 +0530

    patient search created

--- controller/patientrouter.js
+++ controller/patientrouter.js
@@ -1,35 +1,18 @@
-const express=require("express")
-const patientrouter=require("../model/patientmodel")
+const express = require("express");
+const patientrouter = require("../model/patientmodel");
 
-const router=express.Router()
+const router = express.Router();
 
+router.post("/entry", async (req, res) => {
+  let data = req.body;
+  let patient = new patientrouter(data);
+  let result = await patient.save();
+  res.json({ status: "success" });
+});
 
+router.get("/view", async (req, res) => {
+  let data = await patientrouter.find();
+  res.json(data);
+});
 
-
-router.post("/entry",async(req,res)=>{
-    
-    
-    let data=req.body
-    let patient=new patientrouter(data)
-    let result=await patient.save()    
-    res.json({status:"success"})})
- 
-    router.get("/view",async(req,res)=>{
-        let data=await patientrouter.find()
-        res.json(data)
-    
-    })
-
-
-
-   
-
-
-module.exports=router
-
-
-
-
-
-
-
+module.exports = router;
