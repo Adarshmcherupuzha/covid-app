cec160d42ee97525f4efd3254bf9f000cc431ca6
Adarshmcherupuzha <adarshmpadichal@gmail.com>
2024-02-08 15:34:25 +0530

    patient view added

--- controller/patientrouter.js
+++ controller/patientrouter.js
@@ -13,7 +13,12 @@ router.post("/entry",async(req,res)=>{
     let patient=new patientrouter(data)
     let result=await patient.save()    
     res.json({status:"success"})})
-
+ 
+    router.get("/view",async(req,res)=>{
+        let data=await patientrouter.find()
+        res.json(data)
+    
+    })
 
 
 
